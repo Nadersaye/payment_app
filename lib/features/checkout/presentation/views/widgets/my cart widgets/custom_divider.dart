6852986db34d71aecb32345ea54bfe0b50a27292
Nader <nsyd1582@gmail.com>
2023-10-23 18:51:41 +0300

    create custom divider

---
+++ lib/features/checkout/presentation/views/widgets/my cart widgets/custom_divider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(
+      thickness: 2,
+      indent: 15,
+      endIndent: 15,
+      color: Colors.grey,
+    );
+  }
+}
