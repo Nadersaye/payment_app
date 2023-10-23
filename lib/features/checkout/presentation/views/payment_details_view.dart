0faf68f13cfc554b4e6ea24387497570811dfc16
Nader <nsyd1582@gmail.com>
2023-10-23 20:04:10 +0300

    create custom appbar &payment details view body

--- lib/features/checkout/presentation/views/payment_details_view.dart
+++ lib/features/checkout/presentation/views/payment_details_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/features/checkout/presentation/views/widgets/custom_appbar.dart';
+import 'widgets/payment details widgets/payment_view_body.dart';
 
 class PaymentDetailsView extends StatelessWidget {
   const PaymentDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: customAppbar(context: context, appbarTitle: 'Payment Details'),
+      body: const PaymentDetailsViewBody(),
+    );
   }
 }
