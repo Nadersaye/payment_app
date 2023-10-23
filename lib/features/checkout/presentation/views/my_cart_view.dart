989db646e33b2bf62122b01e1482e781ed3cc968
Nader <nsyd1582@gmail.com>
2023-10-23 12:19:00 +0300

    import image & create my cart appbar

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:payment_app/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'My Cart',
+          style: Styles.style25,
+        ),
+        leading: Center(child: SvgPicture.asset('assets/images/Arrow 1.svg')),
+      ),
+    );
   }
 }
