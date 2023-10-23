596043f888803bcd32a92519561e60d2470af56d
Nader <nsyd1582@gmail.com>
2023-10-23 12:06:08 +0300

    create styles file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:payment_app/features/checkout/presentation/views/my_cart_view.dart';
 
 void main() {
@@ -10,9 +11,14 @@ class PaymentApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyCartView(),
+    return const ScreenUtilInit(
+      designSize: Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyCartView(),
+      ),
     );
   }
 }
