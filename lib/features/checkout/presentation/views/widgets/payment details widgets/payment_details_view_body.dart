5568c35dcf77bf58170ffaed0b5d0d6d09421541
Nader <nsyd1582@gmail.com>
2023-10-24 11:30:35 +0300

    validate credit card input

--- lib/features/checkout/presentation/views/widgets/payment details widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment details widgets/payment_details_view_body.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:payment_app/features/checkout/presentation/views/payment_details_view.dart';
 import 'package:payment_app/features/checkout/presentation/views/widgets/custom_button.dart';
 import 'custom_credit_card.dart';
 import 'payment_methods_listview.dart';
 
-class PaymentDetailsViewBody extends StatelessWidget {
+class PaymentDetailsViewBody extends StatefulWidget {
   const PaymentDetailsViewBody({super.key});
+
+  @override
+  State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
+}
+
+class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
-        SliverToBoxAdapter(child: CustomPaymentMethodsListview()),
-        SliverToBoxAdapter(child: CustomCreditCard()),
+        const SliverToBoxAdapter(child: CustomPaymentMethodsListview()),
+        SliverToBoxAdapter(
+            child: CustomCreditCard(
+          formKey: formKey,
+          autovalidateMode: autovalidateMode,
+        )),
         SliverFillRemaining(
           hasScrollBody: false,
           child: Align(
             alignment: Alignment.bottomCenter,
             child: CustomButton(
               buttonText: 'Pay',
-              navigationWidget: PaymentDetailsView(),
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                  setState(() {});
+                } else {
+                  autovalidateMode = AutovalidateMode.always;
+                }
+              },
             ),
           ),
         )
