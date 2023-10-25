d8c4104ca1639f7a82ba5fef321c3d7421db8e55
Nader <nsyd1582@gmail.com>
2023-10-25 19:07:37 +0300

    create initPaymentSheet &displayPaymentSheet

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -2,8 +2,9 @@ import 'package:payment_app/core/utils/api_service.dart';
 import 'package:payment_app/core/utils/payment_keys.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
-class PaymentService {
+class StripeService {
   final ApiService apiService = ApiService();
   Future<PaymentIntentModel> createPaymentIntent(
       PaymentIntentInputModel paymentIntentInputModel) async {
@@ -13,5 +14,16 @@ class PaymentService {
         url: 'https://api.stripe.com/v1/payment_intents');
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+            paymentIntentClientSecret: paymentIntentClientSecret,
+            merchantDisplayName: 'nader'));
+  }
+
+  Future displayPaymentSheet() async {
+    Stripe.instance.presentPaymentSheet();
   }
 }
