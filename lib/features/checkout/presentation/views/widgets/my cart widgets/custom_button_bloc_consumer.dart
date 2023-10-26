895f6b89e0b7002e60340cccae3df6132b167383
Nader <nsyd1582@gmail.com>
2023-10-26 19:37:32 +0300

    create amount model &update flutter & dart version

--- lib/features/checkout/presentation/views/widgets/my cart widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/presentation/views/widgets/my cart widgets/custom_button_bloc_consumer.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment_app/features/checkout/presentation/manager/cubit/payment_cubit.dart';
 import 'package:payment_app/features/checkout/presentation/views/thank_you_view.dart';
-
-import '../../../../data/models/payment_intent_input_model.dart';
+import 'dart:developer';
 import '../custom_button.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
@@ -32,13 +32,75 @@ class CustomButtonBlocConsumer extends StatelessWidget {
             isLoading: state is PaymentLoading ? true : false,
             buttonText: 'Continue',
             onPressed: () {
-              PaymentIntentInputModel paymentIntentInputModel =
+              /*PaymentIntentInputModel paymentIntentInputModel =
                   PaymentIntentInputModel(
                       amount: (100 * 100).toString(),
                       currency: 'EGP',
                       customerId: 'cus_OtBbIs3aWW3vWQ');
               BlocProvider.of<PaymentCubit>(context).makePayment(
-                  paymentIntentInputModel: paymentIntentInputModel);
+                  paymentIntentInputModel: paymentIntentInputModel);*/
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '100',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '100',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+
+                        // Optional
+                        //   "shipping_address": {
+                        //     "recipient_name": "Tharwat samy",
+                        //     "line1": "tharwat",
+                        //     "line2": "",
+                        //     "city": "tharwat",
+                        //     "country_code": "EG",
+                        //     "postal_code": "25025",
+                        //     "phone": "+00000000",
+                        //     "state": "ALex"
+                        //  },
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    log('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
             });
       },
     );
